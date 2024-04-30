782c6d19da740de45a368c1312ebdcefa8614ba2
v.bazhenov <sleem81@list.ru>
2024-04-30 11:28:36 +0500

    UserServiceImpl - внутри класса создаём экземпляр UserDaoJDBCImpl

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+
+    UserDaoJDBCImpl dop = new UserDaoJDBCImpl(); // добавил
+
     public void createUsersTable() {
 
     }
